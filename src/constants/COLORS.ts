c26e5d94e2ed0cfb9a49d1838b62092027811fc4
Abdullah Elseginy <abdullah.ahmed.elseginy@gmail.com>
2024-11-13 17:54:44 +0200

    change constant colors

--- src/constants/COLORS.ts
+++ src/constants/COLORS.ts
@@ -1,23 +1,49 @@
 export const COLORS = {
   black: '#000000',
   white: '#FFFFFF',
-  primary: '#0CCE68',
-  primaryLight: '#88FF99',
-  primaryMoreLight: '#88ff9a5b',
+  primary: '#85c3ff',
+  primaryLight: '#b6dbff',
+  primaryMoreLight: '#85c2ff65',
   grayLight: '#B4B9C9',
   grayMoreLight: '#F1F1F1',
   blueLight: '#F0F5FB',
   yellowlight: '#FCF6D9',
   yellow: '#e1d123',
-  blue: '#4d6182',
+  blue: '#0054d8',
   bluesmothy: '#186dc92e',
   bluesmothy2: '#4d6182',
   blueMoresmothy: '#186dc90e',
-  GreenSmothy: '#53b427',
-  GreenSmothy2: '#54b42734',
+  GreenSmothy: '#4d6182',
+  GreenSmothy2: '#2a3c5b3d',
   danger: '#e51b1168',
   red: '#7f0101',
   Overlay: '#00000031',
   gray: '#949393',
   dangerLight: '#e51c1126',
+
+
+
 };
+
+
+// :root {
+//   --primary: #85c3ff;
+//   --secondry: #b6dbff;
+//   --blue:#0054d8;
+//   --third: #e6f2ff;
+//   --white: #ffffff;
+//   --black: #282828;
+//   --dark-gray: #323232;
+//   --gray: #80808033;
+//   --gray2: #b9b9b933;
+//   --gray3: #ababab;
+//   --gray4: #f6f6f6;
+//   --light-gray: #ffffff5e;
+//   --light-blue: #e6f2ff;
+//   --blue-secondry: #4d6082;
+//   --primary-blue : #85c4ff;
+//   --btn-hover : #bfd6ff
+// }
+
+
+
